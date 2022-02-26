c1968403f58d40fff71cac3e9a1e0b44d2a59fd3
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-02-26 13:25:20 -0300

    Add função para erro de servidor e para debug

--- node-str/server.js
+++ node-str/server.js
@@ -3,9 +3,10 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
+const { builtinModules } = require('module');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || 3000);
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -17,5 +18,56 @@ const route = router.get('/', (req, res, next)=> {
         version: "0.0.1"
     });
 });
-
 app.use('/', route);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+console.log('Api rodando na porta' + port);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)) {
+        return val;
+    };
+
+    if(port >= 0) {
+        return port
+    };
+
+    return false;
+};
+
+function onError(error) {
+    if(error.syscall !== 'listen') {
+        throw error;
+    };
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Pipe ' + port;
+    
+    switch(error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default: 
+            throw error;
+        
+    };
+};
+
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string'
+        ? 'pipe ' + addr
+        : 'pipe ' + addr.port;
+    debug('Listening on ' + bind);
+};
